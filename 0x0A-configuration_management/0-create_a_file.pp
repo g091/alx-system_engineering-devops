667d81961a8d9f431ebb09a67f4b7ae2d1a3a76f
g091 <gotieno791@gmail.com>
2022-09-16 19:07:31 +0300

    t0_create file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # create a file in the /tmp directory
 file {'/tmp/school':
-	ensure  => 'file',
-	mode    => '0744',
-	owner   => 'www-data',
-	group   => 'www-data',
-	content => 'I love Puppet',
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
