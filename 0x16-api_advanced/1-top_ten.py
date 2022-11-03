96e5126827f7b2c53ce8bdaff1baed33640d32da
g091 <gotieno791@gmail.com>
2022-11-04 00:26:26 +0300

    top 10

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Function that queries the Reddit API & prints the titles
+of the first 10 hot posts listed for a given subreddit."""
+
+import requests
+
+
+def top_ten(subreddit):
+    """Prints the titles of the top 10"""
+    if subreddit is None or type(subreddit) is not str:
+        print(None)
+    r = requests.get('http://www.reddit.com/r/{}/hot.json'.format(subreddit),
+                     headers={'User-Agent': 'Python/requests:APIproject:\
+                     v1.0.0 (by /u/aaorrico23)'},
+                     params={'limit': 10}).json()
+    posts = r.get('data', {}).get('children', None)
+    if posts is None or (len(posts) > 0 and posts[0].get('kind') != 't3'):
+        print(None)
+    else:
+        for post in posts:
+            print(post.get('data', {}).get('title', None))
